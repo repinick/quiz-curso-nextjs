574506fb2c280069615323780d626c96b40f7e9f
Nicoli Ferreira <nicoli.ferreira@unifesp.br>
2023-04-12 13:05:37 -0300

    [Chore] Vercel URL updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Questionario from '../components/Questionario'
 import { useRouter } from 'next/router'
 
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://quiz-curso-nextjs.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
